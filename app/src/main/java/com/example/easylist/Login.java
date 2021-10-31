7fdce82353abbf35bd8a62fd567b0de4b11af65d
Michel Fawaz <Michelfawaz87@gmail.com>
2021-10-31 13:59:39 +0200

    Login and Register Linked with firebase

--- app/src/main/java/com/example/easylist/Login.java
+++ app/src/main/java/com/example/easylist/Login.java
@@ -1,21 +1,34 @@
 package com.example.easylist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
     private EditText LoginEmail, LoginPassword;
     private Button LoginButton;
     private TextView HaveAcc;
     private Toolbar toolbar;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog loader;
+
 
 
     @Override
@@ -35,6 +48,9 @@ public class Login extends AppCompatActivity {
 
         getSupportActionBar().setTitle("Login");
 
+        mAuth = FirebaseAuth.getInstance();
+        loader = new ProgressDialog(this);
+
 
 
         HaveAcc.setOnClickListener(new View.OnClickListener() {
@@ -42,6 +58,50 @@ public class Login extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(Login.this, RegisterActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = LoginEmail.getText().toString().trim();
+                String password = LoginPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    LoginEmail.setError("Cannot Be Empty");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    LoginPassword.setError("Cannot Be Empty");
+                    return;
+
+                }
+                else{
+                    loader.setMessage("Logging in...");
+                    loader.setCanceledOnTouchOutside(false);
+                    loader.show();
+
+                    mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Intent intent = new Intent(Login.this,Home.class);
+                                startActivity(intent);
+                                finish();
+                                loader.dismiss();
+
+                            }
+                            else{
+                                String error = task.getException().toString();
+                                Toast.makeText(Login.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                                loader.dismiss();
+                            }
+                        }
+                    });
+
+                }
+
+
             }
         });
 
--- app/src/main/java/com/example/easylist/RegisterActivity.java
+++ app/src/main/java/com/example/easylist/RegisterActivity.java
@@ -1,21 +1,35 @@
 package com.example.easylist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
-    private EditText RegisterEmail, RegisterPassword;
-    private Button RegisterButton;
+    private EditText RgstEmail, RgstPassword;
+    private Button RgstBtn;
     private TextView HaveAccLog;
     private Toolbar toolbar;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog loader;
+
 
 
 
@@ -28,16 +42,66 @@ public class RegisterActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.RegisterTb);
         getSupportActionBar().setTitle("Register");
 
-        RegisterEmail = findViewById(R.id.RegisterEmail);
-        RegisterPassword = findViewById(R.id.RegisterPassword);
-        RegisterButton = findViewById(R.id.RegisterButton);
+        RgstEmail = findViewById(R.id.RegisterEmail);
+        RgstPassword = findViewById(R.id.RegisterPassword);
+        RgstBtn = findViewById(R.id.RegisterButton);
         HaveAccLog = findViewById(R.id.HaveAccountLog);
+
+
+        //firebase
+        mAuth = FirebaseAuth.getInstance();
+
+        loader = new ProgressDialog(this);
+
 
         HaveAccLog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(RegisterActivity.this, Login.class);
                 startActivity(intent);
+            }
+        });
+
+
+        RgstBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = RgstEmail.getText().toString().trim();
+                String password = RgstPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    RgstEmail.setError("Cannot be Empty");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    RgstPassword.setError("Cannot be Empty");
+                    return;
+                }
+                else{
+                    loader.setMessage("Registering...");
+                    loader.setCanceledOnTouchOutside(false);
+                    loader.show();
+                    mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(RegisterActivity.this, Home.class);
+                                startActivity(intent);
+                                finish();
+                                loader.dismiss();
+                            } else {
+                                String error = task.getException().toString();
+                                Toast.makeText(RegisterActivity.this, "Failed registration", Toast.LENGTH_SHORT).show();
+                                loader.dismiss();
+                            }
+                        }
+                    });
+                }
+
+
+
+
+
             }
         });
 
