46ac46874582ddc977ea64b415491b249b43d031
Michel Fawaz <Michelfawaz87@gmail.com>
2021-11-02 15:01:51 +0200

    .

--- app/src/main/java/com/example/easylist/MainActivity.java
+++ app/src/main/java/com/example/easylist/MainActivity.java
@@ -1,51 +1,31 @@
 package com.example.easylist;
 
+import static androidx.navigation.Navigation.findNavController;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
-    private static final int SPLASH =3300;
-
-    Animation LogoAnim, TextAnim;
-    ImageView imageView;
-    TextView textView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
-
-        LogoAnim = AnimationUtils.loadAnimation( this, R.anim.splash_animation);
-        TextAnim = AnimationUtils.loadAnimation(  this, R.anim.splashanimationtext);
-        imageView = findViewById(R.id.imgview);
-        textView = findViewById(R.id.maintxt);
-
-        imageView.setAnimation(LogoAnim);
-        textView.setAnimation(TextAnim);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, RegisterActivity.class );
-                startActivity(intent);
-                finish();
-
-            }
-        }, SPLASH);
-
 
     }
-
-
-
 }
+
